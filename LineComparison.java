42a237da36d97741344ad1cf200c1aa29e592f44
Mohit Shah <mohitshah7777@gmail.com>
2021-04-02 22:50:43 +0530

    Coordinates are taken as input and length has been calculated

--- LineComparison.java
+++ LineComparison.java
@@ -1,6 +1,34 @@
+import java.util.Scanner;
+
 public class LineComparison {
 
 	public static void main(String[] args) {
 
+		System.out.println("Enter coordinates of line 1");
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter x1, x2, y1, y2 = ");
+		int ax1=scan.nextInt();
+		int ax2=scan.nextInt();
+		int ay1=scan.nextInt();
+		int ay2=scan.nextInt();
+		
+		int diff_ax=ax2-ax1;
+		int power=2;
+		int diff_ay=ay2-ay1;
+		Double length1=Math.sqrt(Math.pow(diff_ax,power) + Math.pow(diff_ay,power));
+		System.out.println("Length of line 1 is = " + length1);	
+	
+		System.out.println("Enter coordinates of line 2");		
+		System.out.print("Enter x1, x2 ,y1 ,y2 = ");
+		int bx1=scan.nextInt();
+		int bx2=scan.nextInt();
+		int by1=scan.nextInt();
+		int by2=scan.nextInt();
+		
+		int diff_bx=bx2-bx1;
+		int diff_by=by2-by1;
+		Double length2=Math.sqrt(Math.pow(diff_bx,power) + Math.pow(diff_by,power));
+		System.out.println("Length of line 2 is = " + length2);	
 	}
+
 }
